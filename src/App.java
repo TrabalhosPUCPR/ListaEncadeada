ff9f001d59152f17234e84b5d071214db679048e
KnightLeo <80933008+Knightleo602@users.noreply.github.com>
2022-04-07 15:21:46 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,14 +1,37 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Lista lista_ordenada = new Lista();
-        lista_ordenada.add(7);
-        lista_ordenada.add(5);
-        lista_ordenada.add(6);
-        lista_ordenada.add(2);
-        lista_ordenada.add(3);
-        lista_ordenada.add(1);
-        lista_ordenada.add(4);
-        System.out.print("LISTA ORDENADA: ");
+        lista_ordenada.add_ordenado(1);
+        lista_ordenada.add_ordenado(10);
+        lista_ordenada.add_ordenado(11);
+        lista_ordenada.add_ordenado(7);
+        lista_ordenada.add_ordenado(8);
+        lista_ordenada.add_ordenado(5);
+        lista_ordenada.add_ordenado(4);
+        System.out.print("LISTA ORDENADA 1: ");
+        lista_ordenada.print();
+
+        Lista lista_ordenada2 = new Lista();
+        lista_ordenada2.add_ordenado(100);
+        lista_ordenada2.add_ordenado(3787);
+        lista_ordenada2.add_ordenado(5657);
+        lista_ordenada2.add_ordenado(8577);
+        lista_ordenada2.add_ordenado(6567);
+        lista_ordenada2.add_ordenado(9454);
+        lista_ordenada2.add_ordenado(10564);
+
+        System.out.print("LISTA ORDENADA 2: ");
+        lista_ordenada2.print();
+        System.out.println("cos(x,y): " + lista_ordenada.similaridade(lista_ordenada2));
+
+        System.out.print("LISTA ORDENADA 1 (primeiro elemento removido): ");
+        lista_ordenada.remove_primeiro();
+        lista_ordenada.print();
+        System.out.print("LISTA ORDENADA 1 (ultimo elemento removido): ");
+        lista_ordenada.remove_ultimo();
+        lista_ordenada.print();
+        System.out.print("LISTA ORDENADA 1 (elemento depois do index x removido): ");
+        lista_ordenada.remove_depois(4); // pode colocar index ou o No como argumento, ele faz o get automatico
         lista_ordenada.print();
     }
 }
--- src/Lista.java
+++ src/Lista.java
@@ -7,6 +7,18 @@ public class Lista {
         ultimo = null;
     }
 
+    public No get(int i){
+        No p = this.primeiro;
+        for(int y = 0; p != this.ultimo; y++){
+            if(y == i){
+                return p;
+            }
+            p = p.proximo;
+        }
+        System.out.println("Index invalido");
+        return null;
+    }
+
     public void print(){
         No p = this.primeiro;
         System.out.print("[");
@@ -18,33 +30,101 @@ public class Lista {
         System.out.println("]");
     }
 
-    public void add(int dado){add(dado, true);}
-    public void add(int dado, boolean ordenado){
+    public boolean vazio(){
+        return this.primeiro == null;
+    }
+
+    public double similaridade(Lista lista){
+        No p = this.primeiro;
+        No p2 = lista.primeiro;
+        double prod_vetorial = 0;
+        double quad_todos1 = 0;
+        double quad_todos2 = 0;
+        while(p != null){
+            prod_vetorial += p.dado * p2.dado;
+            quad_todos1 += p.dado*p.dado;
+            quad_todos2 += p2.dado*p2.dado;
+            p = p.proximo;
+            p2 = p2.proximo;
+        }
+        return (prod_vetorial/Math.sqrt(quad_todos1 * quad_todos2));
+    }
+
+    public void remove_primeiro(){
+        if(!vazio()){
+            this.primeiro = this.primeiro.proximo;
+        }
+    }
+    public void remove_ultimo(){
+        No p = this.primeiro;
+        if(!vazio()){
+            while(p != this.ultimo){
+                if(p.proximo == this.ultimo){
+                    this.ultimo = p;
+                    this.ultimo.proximo = null;
+                }else{
+                    p = p.proximo;
+                }
+            }
+        }
+    }
+
+    public void remove_depois(int index){remove_depois(this.get(index));}
+    public void remove_depois(No no){
+        No p = this.primeiro;
+        while(p != null){
+            if(p == no){
+                p.proximo = p.proximo.proximo;
+                return;
+            }else{
+                p = p.proximo;
+            }
+        }
+        System.out.println("Valor inserido nao encontrado");
+    }
+
+    public void add_primeiro(int dado){
+        No p = new No(dado);
+        p.proximo = this.primeiro;
+        this.primeiro = p;
+    }
+    public void add_ultimo(int dado){
+        No p = new No(dado);
+        this.ultimo.proximo = p;
+        this.ultimo = this.ultimo.proximo;
+    }
+    public void add_depois(No no, int dado){
+        No p = this.primeiro;
+
+        while(p != no){
+            if(p == no){
+                No no2 = new No(dado);
+                no2.proximo = p.proximo;
+                p.proximo = no2;
+                return;
+            }
+        }
+        System.out.println("No nao encontrado na lista");
+    }
+    public void add_ordenado(int dado){
         No p = this.primeiro;
         if(p != null){
-            if(ordenado){ // adiciona de forma ordenada
-                if(p.dado > dado){ // caso for menor que o primeiro, coloca por primeiro (ou se tiver vazio)
-                    this.primeiro = new No(dado);
-                    this.primeiro.proximo = p;
-                }else if(this.ultimo.dado < dado){ // caso for maior que o ultimo, coloca como o ultimo
-                    p = this.ultimo;
-                    this.ultimo.dado = dado;
-                    this.ultimo.proximo = p;
-                }else{
-                    while(p.dado < dado){ // verifica todas, e coloca onde o proximo for maior
-                        if(p.proximo.dado > dado){
-                            No a = new No(dado);
-                            a.proximo = p.proximo;
-                            p.proximo = a;
-                        }else{
-                            p = p.proximo;
-                        }
-                    }
-                }
-            }else{
-                //ESSE SO ADICIONA NA ULTIMA POSICAO (NAO ORDENADO)
+            if(p.dado > dado){ // caso for menor que o primeiro, coloca por primeiro (ou se tiver vazio)
+                this.add_primeiro(dado);
+            }else if(this.ultimo.dado < dado){ // caso for maior que o ultimo, coloca como o ultimo
                 this.ultimo.proximo = new No(dado);
                 this.ultimo = this.ultimo.proximo;
+                
+            }else{
+                while(p.dado < dado){ // verifica todas, e coloca onde o proximo for maior
+                    if(p.proximo.dado > dado){
+                        No a = new No(dado);
+                        a.proximo = p.proximo;
+                        p.proximo = a;
+                    }else{
+                        p = p.proximo;
+                    }
+                }
             }
         }else{
             this.primeiro = new No(dado);
